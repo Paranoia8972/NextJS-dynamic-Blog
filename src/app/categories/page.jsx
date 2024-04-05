ddd14cf1043c98b1af56d3c76f9d4990bce88226
Paranoia8972 <contact@encryptopia.dev>
2024-04-05 13:53:59 +0200

    Minor improvements

--- src/app/categories/page.jsx
+++
@@ -1,8 +0,0 @@
-import Link from "next/link";
-import CategoryList from "@/components/CategoryList";
-
-export default function CategoryPage({ }) {
-    return (
-        <CategoryList />
-    );
-}
